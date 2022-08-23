944a8710a40b1ab4809762f9a1ed536b02dde3d8
alvaroormeno <alvaro@ormeno.org>
2022-08-23 12:06:13 -0400

    routes.js - implemented first route to get all entries which requires getEntries controller

---
+++ backend/routes/entries.js
@@ -0,0 +1,15 @@
+// FILE MAIN NOTE:
+// - We dont want to clutter server.js by having all of our routes there. Therefore in this file we will have all of our routes which we can export and then import them in server.js.
+////////////////////////////////////////////////////////////////////
+
+// Require express
+const express = require('express');
+// Require express router
+const router = express.Router();
+
+
+// ROUTE 1
+router.get('/' getEntries)
+
+
+module.exports = router
