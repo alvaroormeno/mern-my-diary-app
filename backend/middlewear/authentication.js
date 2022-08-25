e8a221180848342d1c84f31c3becd1a4431aa5bd
alvaroormeno <alvaro@ormeno.org>
2022-08-24 20:34:26 -0400

    completed authentication middlewear

--- backend/middlewear/authentication.js
+++ backend/middlewear/authentication.js
@@ -8,7 +8,37 @@ const requiresAuth = async (req, res, next) => {
   let isAuthed = false
 
   if(token) {
-    res.json({comment: 'auth works'})
+
+    try {
+      
+      // deconstruct token to get userID which is the payload converted into token in login call
+      const { userId } = jwt.verify(token, process.env.JWT_SECRET)
+
+      // use the userid to find the user in database
+      const userFound = await UserModel.findById(userId)
+
+      // if user is found return found user minus password and change is authed to true
+      if(userFound) {
+        req.user = {
+          _id: userFound._id,
+          email: userFound.email,
+          name: userFound.name,
+        }
+        isAuthed = true
+      }
+
+    } catch (error) {
+      // if error catched, is authed = false
+      isAuthed = false
+    }
+
+    // if isauthed is true, then we can continue with the call
+    if(isAuthed) {
+      return next()
+    } else {
+      return res.status(401).send('NOT AUTHORISED')
+    }
+    
   }
 }
 
