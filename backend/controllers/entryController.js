d3ab0461655046f4917b6b5c0c8c7b6adff1d1c4
alvaroormeno <alvaro@ormeno.org>
2022-08-25 00:36:06 -0400

    entrycontroller.js - added notes for createEntry function

--- backend/controllers/entryController.js
+++ backend/controllers/entryController.js
@@ -46,22 +46,26 @@ const getEntries = async (req, res) => {
 
 // Description - CREATE NEW ENTRY
 // Route - PUT /api/entry/new
+// PRIVATE - uses requiresAuth middleware
 const createEntry =  async (req, res) => {
-
   try {
+    // STEP 1 ->
+    // After running requiresAUTH, create a new entry based on 2 properties, user and content.
+    // The user has the value of req.user._id, this data comes from requiresAuth middleware.
     const newEntry = await EntryModel.create({
       user: req.user._id,
       content: req.body.content,
     })
-
+    // STEP 2 ->
+    // Once entry created, respond with a status of 200 and the new entry data.
     return res.status(200).json(newEntry)
 
+  // STEP 3 ->
+  // If error is catched in try section, console.log error and return error status + message.
   } catch (error) {
     console.log("here is error" + error)
     return res.status(500).send(error.message)
-    
   }
-
 }
 
 module.exports = { getEntries, createEntry}
