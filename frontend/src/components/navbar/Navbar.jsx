7d5804e6e3307a21aad20205f4ce497386cca1b5
alvaroormeno <alvaro@ormeno.org>
2022-08-26 16:46:52 -0400

    navbar jsx and style mvp complete

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import {BsJournalBookmark} from 'react-icons/bs'
+
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className={styles.navbar}>
+      {/* Logo + Title Container */}
+      <div className={styles.logo}>
+        <BsJournalBookmark size={40} />
+        <h1>MY DIARY APP</h1>
+      </div>
+      {/* Desktop Menu Container */}
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <a href="">Journaling Benefits</a>
+          </li>
+          <li>
+            <a href="">Login In</a>
+          </li>
+          <li>
+          <a href="">Sign Out</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
