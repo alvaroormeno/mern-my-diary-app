d09ad17051ca0fd8d4c108b4cc3cd07f026254e3
alvaroormeno <alvaro@ormeno.org>
2022-08-26 18:21:55 -0400

    new entry component mvp ready

--- frontend/src/components/newEntry/NewEntry.jsx
+++ frontend/src/components/newEntry/NewEntry.jsx
@@ -4,7 +4,14 @@ import styles from './NewEntry.module.css'
 
 const NewEntry = () => {
   return (
-    <div>NewEntry</div>
+    <form className={styles.new_entry}>
+      <textarea
+        placeholder='Whats on your mind today? ...'
+      >
+
+      </textarea>
+      <button>Save Entry</button>
+    </form>
   )
 }
 
