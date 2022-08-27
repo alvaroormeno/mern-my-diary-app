e1b00fc45cacc8ebe9fef1bdd9f8b7e9448dc0b3
alvaroormeno <alvaro@ormeno.org>
2022-08-27 16:00:13 -0400

    working on globalprovider action ton get current user, need to create controller for that first

--- frontend/src/context/GlobalReducer.js
+++ frontend/src/context/GlobalReducer.js
@@ -4,4 +4,22 @@ import React, {useReducer} from "react";
 ////////////////////////////////////////////////////////////////////
 // GLOBAL REDUCER
 const globalReducer = (state, action ) => {
+
+  switch (action.type) {
+
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        user: action.payload,
+      }
+
+
+    default: 
+      return state;
+  }
+
+
+
 }
+
+export default globalReducer
