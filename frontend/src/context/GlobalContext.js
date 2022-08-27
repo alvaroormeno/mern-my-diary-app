dc927c80256c0cb563b9cfaa06798b198500b8f1
alvaroormeno <alvaro@ormeno.org>
2022-08-27 16:22:24 -0400

    GlobalContext ready for trial

--- frontend/src/context/GlobalContext.js
+++ frontend/src/context/GlobalContext.js
@@ -25,15 +25,45 @@ export const GlobalProvider = (props) => {
 
   const[state, dispatch] = useReducer(globalReducer, initialState)
 
+  useEffect( () => {
+    getCurrentUser()
+  }, [])
+
   // ACTION 1: GET CURRENT USER
   const getCurrentUser = async () => {
     
     try {
 
-      const res = await axios.get('api/')
+      const res = await axios.get('api/user/current')
+
+      if(res.data) {
+        dispatch({type: "SET_CURRENT_USER", payload: res.data })
+      }
+
       
     } catch (error) {
       
     }
   }
+
+
+  // Value for all globalprivder functions
+  const value = {
+    ...state,
+    getCurrentUser
+  }
+
+  return (
+    <GlobalContext.Provider value={value}>
+      {props.children}
+    </GlobalContext.Provider>
+  )
+
+
+
 }
+
+export function useGlobalContext() {
+  return useContext(GlobalContext)
+}
+
